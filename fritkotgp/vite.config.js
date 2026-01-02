bab6a49ff3a9fa34389ed253dc308ec1bf67db82
NolanWeemaels <Nolan.weemaels@student.ehb.be>
2026-01-02 23:08:50 +0100

    tried fixing styling

--- fritkotgp/vite.config.js
+++ fritkotgp/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "/web2-course-project-front-end-NolanWeemaels/",
+  base: "/Web2FritkotGP/",
   build: {
     rollupOptions: {
       input: {
